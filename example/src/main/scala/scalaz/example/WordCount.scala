6a67285dd4d95c876e32f83c316d2dd7ea8160d3
Jason Zaugg <jzaugg@gmail.com>
2014-02-15 21:44:03 +0100

    Comment out failing code using AppFuncU
    See:
    
    https://issues.scala-lang.org/browse/SI-8290
    https://issues.scala-lang.org/browse/SI-7753
    
    The Unapply trick to infer the identity type constructor
    no longer flows through the inferred types.

--- example/src/main/scala/scalaz/example/WordCount.scala
+++ example/src/main/scala/scalaz/example/WordCount.scala
@@ -1,45 +1,45 @@
-package scalaz.example
+// package scalaz.example
 
-/**
- * Character/Line/Word Count from "The Essence of the Iterator Pattern".
- *
- * @see [[http://www.comlab.ox.ac.uk/jeremy.gibbons/publications/iterator.pdf]]
- */
-object WordCount {
-  def main(args: Array[String]) = {
-    wordCount
-  }
+// /**
+//  * Character/Line/Word Count from "The Essence of the Iterator Pattern".
+//  *
+//  * @see [[http://www.comlab.ox.ac.uk/jeremy.gibbons/publications/iterator.pdf]]
+//  */
+// object WordCount {
+//   def main(args: Array[String]) = {
+//     wordCount
+//   }
 
-  def wordCount {
-    import scalaz.typelevel.{AppFunc, AppFuncU, HList, HNil},
-      scalaz.syntax.typelevel.hlist._,
-      scalaz.State._, scalaz.std.anyVal._, scalaz.std.list._,
-      scalaz.std.boolean.test, scalaz.syntax.equal._
+//   def wordCount {
+//     import scalaz.typelevel.{AppFunc, AppFuncU, HList, HNil},
+//       scalaz.syntax.typelevel.hlist._,
+//       scalaz.State._, scalaz.std.anyVal._, scalaz.std.list._,
+//       scalaz.std.boolean.test, scalaz.syntax.equal._
 
-    val text = "the cat in the hat\n sat on the mat\n".toList
+//     val text = "the cat in the hat\n sat on the mat\n".toList
 
-    // To count characters, treat Int as monoidal applicative
-    val countChar = AppFuncU { (c: Char) => 1 }
+//     // To count characters, treat Int as monoidal applicative
+//     val countChar = AppFuncU { (c: Char) => 1 }
 
-    // To count lines, treat Int as monoidal applicative
-    val countLine = AppFuncU { (c: Char) => test(c === '\n') }
+//     // To count lines, treat Int as monoidal applicative
+//     val countLine = AppFuncU { (c: Char) => test(c === '\n') }
 
-    // To count words, we need to detect transitions from whitespace to non-whitespace.
-    val countWord = AppFuncU { (c: Char) =>
-      for {
-        x <- get[Boolean]
-        y = c =/= ' '
-        _ <- put(y)
-      } yield test(y && !x)
-    } @>>> AppFuncU { (x: Int) => x }
+//     // To count words, we need to detect transitions from whitespace to non-whitespace.
+//     val countWord = AppFuncU { (c: Char) =>
+//       for {
+//         x <- get[Boolean]
+//         y = c =/= ' '
+//         _ <- put(y)
+//       } yield test(y && !x)
+//     } @>>> AppFuncU { (x: Int) => x }
 
-    // Compose applicative functions in parallel
-    val countAll = countChar :: countLine :: (countWord consA AppFunc.HNil[Char, Int])
+//     // Compose applicative functions in parallel
+//     val countAll = countChar :: countLine :: (countWord consA AppFunc.HNil[Char, Int])
     
-    // ... and execute them in a single traversal 
-    val charCount :: lineCount ::  wordCountState :: HNil = countAll traverse text
-    val wordCount = wordCountState.eval(false)
+//     // ... and execute them in a single traversal 
+//     val charCount :: lineCount ::  wordCountState :: HNil = countAll traverse text
+//     val wordCount = wordCountState.eval(false)
 
-    println("%d\t%d\t%d\t".format(lineCount, wordCount, charCount)) // 2	9	35
-  }
-}
+//     println("%d\t%d\t%d\t".format(lineCount, wordCount, charCount)) // 2	9	35
+//   }
+// }
